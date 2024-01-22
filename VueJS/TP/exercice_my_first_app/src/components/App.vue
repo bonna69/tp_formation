22b13e5a53e139f12a1f59e1cebc41b57791e70e
Lobna <monemail@mm.com>
2024-01-22 10:21:49 +0100

    Vue exercice - first commit

---
+++ VueJS/TP/exercice_my_first_app/src/components/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+const model = defineModel()
+
+function update(){
+  model.value++
+}
+</script>
+
+<template>
+   <h1>Hello world</h1>
+   <p> </p>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ VueJS/TP/exercice_my_first_app/src/components/icons/Helloworld.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWord from '@/components/icons/Helloworld.vue';
+
+
+
+</script>
+
+
+<template>
+
+
+</template>
+
