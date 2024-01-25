104ff883daf3aa0869511e0aeb07ec6c1acd017f
Lobna <monemail@mm.com>
2024-01-25 10:06:59 +0100

    M.A.J devoir

---
+++ VueJS/TP/exercice_3/src/components/Layout/MainLayout.vue
@@ -0,0 +1,41 @@
+<script>
+    export default {
+        name: "MainLayout"
+    }
+</script>
+<template>
+    <!-- redéfinir les slots  -->
+        <!-- - nommé les slots -->
+        <!-- - rendu consitionnel de slots / expliquer -->
+        <!-- - injecter dans le slot Header -->
+    <div class="container">
+        <header
+            class="p-3 mb-3 border-bottom"
+           v-if="$slots.header"
+        >
+            <slot> name="header"</slot>
+        </header>
+        <main
+            v-if="$slots.default"
+            >
+            <slot></slot>
+        </main>
+        <footer
+        
+          v-if="$slots.footer"
+        >
+        <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-wrap: wrap;
+}
+header, main, footer {
+    width: 100%;
+    min-height: 4rem;
+}
+</style>
